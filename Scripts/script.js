851dcc96913f9475e51e1ee4473661cba36ba9a2
SquishyMona <kotonefemc@gmail.com>
2021-10-04 09:58:02 -0400

    Day 7

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+variable = new Array()
+            variable[0] = "index"
+            variable[1] = "Page1"
+            function newpage(clicked)
+            {
+                p5area.location.href = variable[click] + ".html"
+            }
